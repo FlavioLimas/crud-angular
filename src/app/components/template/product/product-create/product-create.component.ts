e8c96a8021b7de3d25884581aba5697a4015ad08
Flavio Lima <flaviolima.s@live.com>
2023-05-26 17:28:41 -0300

    implementacao de create service

--- src/app/components/template/product/product-create/product-create.component.ts
+++ src/app/components/template/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -8,18 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: 'Produto de Teste',
+    price: 125.98
+  }
+
   constructor(private productService: ProductService,
-    private router: Router) {}
+    private router: Router) { }
 
   ngOnInit(): void {
-    
+
   }
 
   createProduct(): void {
-    this.productService.showMessage('Produto criado com sucesso!');
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!');
+      this.router.navigate(['/products']);
+    });
   }
 
-  cancel(id: number) :void {
+  cancel(id: number): void {
     this.router.navigate(['/products']);
   }
 }
--- src/app/components/template/product/product.service.ts
+++ src/app/components/template/product/product.service.ts
@@ -1,14 +1,18 @@
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { Product } from "./product.model";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
+
+    baseUrl = "http://localhost:3001/products";
+
     constructor(private snackBar: MatSnackBar, 
-        private http: HttpClientModule) { }
+        private http: HttpClient) { }
 
     showMessage(msg: string): void {
         this.snackBar.open(msg, 'X', {
@@ -18,5 +22,7 @@ export class ProductService {
         });
     }
 
-    
+    create(product: Product): Observable<Product> {
+        return this.http.post<Product>(this.baseUrl, product);
+    }
 }
