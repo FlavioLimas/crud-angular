d74501ad28afdee3eddcf48d8a5bda3dd37219fd
Flavio Lima <flaviolima.s@live.com>
2023-05-26 16:32:11 -0300

    implementado botao para criar produto e excluir produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule,
     MatCardModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/template/product/product-create/product-create.component.ts
+++ src/app/components/template/product/product-create/product-create.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+  constructor(private productService: ProductService,
+    private router: Router) {}
 
+  ngOnInit(): void {
+    
+  }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto criado com sucesso!');
+  }
+
+  cancel(id: number) :void {
+    this.router.navigate(['/products']);
+  }
 }
