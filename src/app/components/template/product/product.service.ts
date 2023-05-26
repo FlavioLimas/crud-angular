3b14c289c4916623dc7a0ed6522a5f10adb16371
Flavio Lima <flaviolima.s@live.com>
2023-05-26 17:05:20 -0300

    injetando httpModule no service

--- src/app/components/template/product/product.service.ts
+++ src/app/components/template/product/product.service.ts
@@ -1,17 +1,22 @@
+import { HttpClientModule } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { Product } from "./product.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
-    constructor(private snackBar: MatSnackBar) { }
+    constructor(private snackBar: MatSnackBar, 
+        private http: HttpClientModule) { }
 
-    showMessage(msg: string) : void {
+    showMessage(msg: string): void {
         this.snackBar.open(msg, 'X', {
             duration: 3000,
             horizontalPosition: "right",
             verticalPosition: "top"
         });
     }
+
+    
 }
