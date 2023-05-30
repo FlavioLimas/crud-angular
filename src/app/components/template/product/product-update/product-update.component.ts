38ee322f2c5ddca9b9bdcda9016cca7495bc8715
Flavio Lima <flaviolima.s@live.com>
2023-05-30 18:32:17 -0300

    definicao de modulo

---
+++ src/app/components/template/product/product-update/product-update.component.ts
@@ -0,0 +1,35 @@
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: .0
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!');
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancel(id: number): void {
+    this.router.navigate(['/products']);
+  }
+}
