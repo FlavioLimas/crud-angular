70059cb47a4fae703660309f2bcf4530a1f28cf8
Flavio Lima <flaviolima.s@live.com>
2023-06-13 18:32:49 -0300

    implementado deleteById

--- src/app/components/template/product/product-read/product-read.component.ts
+++ src/app/components/template/product/product-read/product-read.component.ts
@@ -1,6 +1,8 @@
 import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-read',
@@ -12,11 +14,24 @@ export class ProductReadComponent implements OnInit {
   products: Product[] = [];
   displayedColumns = ['id', 'name', 'price', 'action'];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  deletById(id:string): void {
+    this.productService.deleteById(id).subscribe(() => {
+      this.productService.showMessage("Produto Deletado!");
+      this.getAll();
+    });
+  }
+
+  getAll(): void {
     this.productService.getAll().subscribe(products => {
       this.products = products;
-    })
+    });
   }
 }
